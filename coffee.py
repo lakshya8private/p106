f69315ac213bb90d60f3582ce43c6bf042e1eead
lakshya8private <105441467+lakshya8private@users.noreply.github.com>
2022-06-25 20:26:26 +0530

    Add files via upload

---
+++ coffee.py
@@ -0,0 +1,39 @@
+import plotly.express as px
+import csv
+import numpy as np
+
+
+def plotFigure(data_path):
+    with open(data_path) as csv_file:
+        df = csv.DictReader(csv_file)
+        fig = px.scatter(df, x="Coffee in ml", y="sleep in hours")
+        fig.show()
+
+
+def getDataSource(data_path):
+    marks_in_percentage = []
+    days_present = []
+    with open(data_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            marks_in_percentage.append(float(row["Coffee in ml"]))
+            days_present.append(float(row["sleep in hours"]))
+
+    return {"x": marks_in_percentage, "y": days_present}
+
+
+def findCorrelation(datasource):
+    correlation = np.corrcoef(datasource["x"], datasource["y"])
+    print("Correlation between Cups of Coffee and Hours of Sleep :-  \n--->",
+          correlation[0, 1])
+
+
+def setup():
+    data_path = "cups of coffee vs hours of sleep.csv"
+
+    datasource = getDataSource(data_path)
+    findCorrelation(datasource)
+    plotFigure(data_path)
+
+
+setup()
